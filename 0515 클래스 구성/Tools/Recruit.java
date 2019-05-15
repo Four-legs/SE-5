91e3867447a73734a05441df1f42611454920154
Malliwan <dlcoguswkd@naver.com>
2019-05-16 00:20:27 +0900

    Update Recruit.java

--- 0515 클래스 구성/Tools/Recruit.java
+++ 0515 클래스 구성/Tools/Recruit.java
@@ -1,7 +1,7 @@
 package Tools;
 
 public class Recruit {
-	//this class actually related with recruit scholarship.
+	//this class actually related with recruiting scholarship.
 	//Manager Operation can recruit (each notices)
 	//has information of scholarships, list of students who requested each scholarships.
 	//has information of 
